b8c054f0020f4408dc966a041189dd96c30e1689
Setsuna Yukiii <siang-meng@live.com>
2023-12-24 15:37:37 +0800

    integration of frontend & backend

--- backend/index.ts
+++ backend/index.ts
@@ -1,4 +1,5 @@
 import express, { Application, Router, Request, Response } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import getDatabase from "./database/getDatabase";
 import getAllDatabaseRows from "./database/getAllDatabaseRows";
@@ -9,11 +10,16 @@ const app: Application = express();
 var router: Router = require("express").Router();
 const port: number = 3000;
 app.use(router);
+app.use(cors());
+app.use(express.json());
 
-router.get("/", async function (req: Request, res: Response): Promise<void> {
+router.get("/", function (req: Request, res: Response): void {
     try {
+        res.set("Access-Control-Allow-Origin", "*");
+
         res.send({ message: "Hello world" });
     } catch {
+        console.log("GG");
         res.status(500).send({
             message: "Internal Server Error",
         });
@@ -22,14 +28,17 @@ router.get("/", async function (req: Request, res: Response): Promise<void> {
 
 router.get(
     "/projects",
-    async function (req: Request, res: Response): Promise<void> {
+    async function (req: Request, res: Response): Promise<any> {
         try {
+            res.set("Access-Control-Allow-Origin", "*");
+
             const getProjects = await getDatabase(
                 process.env.NOTION_DATABASE_PROJECTS
             );
             const data = getAllDatabaseRows(getProjects);
             res.send(data);
         } catch {
+            console.log("GG");
             res.status(500).send({
                 message: "Internal Server Error",
             });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const getProjects = async () => {
+        try {
+            const projects = await axios.get("http://localhost:3000/projects");
+            console.info("projects", projects.data);
+        } catch (err) {
+            console.log("err", err);
+        }
+    };
+    useEffect(() => {
+        getProjects();
+    }, []);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return <></>;
 }
 
-export default App
+export default App;
