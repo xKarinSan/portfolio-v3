c7df616639f510b0dc2c46365ecf4744c2e879a6
Setsuna Yukiii <siang-meng@live.com>
2024-01-03 20:49:15 +0800

    add checkbox property

--- backend/databaseHelpers/general/extractProperty.ts
+++ backend/databaseHelpers/general/extractProperty.ts
@@ -2,6 +2,9 @@
 export default function extractProperty(property: any) {
     try {
         const { type: propertyType } = property;
+        console.log("propertyType", propertyType);
+        console.log("property", property);
+        console.log("");
         switch (propertyType) {
             case "rich_text":
                 return property.rich_text[0].plain_text;
@@ -13,6 +16,8 @@ export default function extractProperty(property: any) {
                 return property.number;
             case "date":
                 return property.date.start;
+            case "checkbox":
+                return property.checkbox;
             default:
                 return null;
         }
