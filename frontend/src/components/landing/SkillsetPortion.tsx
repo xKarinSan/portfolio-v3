7522874bd6789ab6d443e9d91bcca753e64de073
Setsuna Yukiii <siang-meng@live.com>
2024-01-04 22:04:02 +0800

    frontend: skills

--- frontend/src/components/landing/SkillsetPortion.tsx
+++ frontend/src/components/landing/SkillsetPortion.tsx
@@ -1,16 +1,41 @@
-import { Card, Heading } from "@chakra-ui/react";
-
+import { Card, Heading, Image, SimpleGrid, Box } from "@chakra-ui/react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 export default function SkillSetPortion() {
+    const [skillsets, setSkillsets] = useState([]);
+    useEffect(() => {
+        axios.get("http://localhost:3000/skills").then((res) => {
+            setSkillsets(res.data);
+        });
+    }, []);
     return (
         <Card
             id="cardd"
-            // height will be adjusted accordingly
-            height="50vh"
+            // height="50vh"
             transform={"translate(0,-50px)"}
             padding="10px"
-            margin="20px"
+            margin="20px auto"
+            width={["80%", null, "60%"]}
         >
             <Heading fontSize={28}>My Skillsets</Heading>
+            <Box padding="20px auto" width={["80%","60%"]} margin="auto">
+                <SimpleGrid columns={[3,4]} gap={1} margin="auto">
+                    {skillsets
+                        .filter((skillset: any) => {
+                            return skillset.toFeature === true;
+                        })
+                        .map((skillset: any) => {
+                            const { name, logoUrl } = skillset;
+                            return (
+                                <Image
+                                    src={logoUrl}
+                                    alt={name}
+                                    margin="30px auto"
+                                />
+                            );
+                        })}
+                </SimpleGrid>
+            </Box>
         </Card>
     );
 }
