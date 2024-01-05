59559ac2583d4021587e9f7a081b07059bfd57cd
Setsuna Yukiii <siang-meng@live.com>
2024-01-05 18:36:57 +0800

    changing of carousel

--- frontend/src/components/landing/ProjectsPortion.tsx
+++ frontend/src/components/landing/ProjectsPortion.tsx
@@ -1,24 +1,39 @@
 import { Box, Heading, Text } from "@chakra-ui/react";
 import ProjectCard from "./cards/ProjectCard";
-import {
-    Carousel,
-    CarouselContent,
-    CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
-} from "../ui/carousel";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Project from "../../types/ProjectType";
 
 export default function ProjectsPortion() {
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 1,
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 1,
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 1,
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1,
+        },
+    };
+
     const [projects, setProjects] = useState<Project[]>([]);
     useEffect(() => {
-        axios
-            .get(import.meta.env.VITE_API_LINK + "projects")
-            .then((res) => {
-                setProjects(res.data);
-            });
+        axios.get(import.meta.env.VITE_API_LINK + "projects").then((res) => {
+            console.log("projects", res.data);
+            setProjects(res.data);
+        });
     }, []);
     return (
         <Box margin="auto" width={["80%"]}>
@@ -26,27 +41,17 @@ export default function ProjectsPortion() {
             <Text>
                 Here are the projects I’ve worked on. Click here to see more
             </Text>
-            <Carousel>
-                <CarouselPrevious />
-                <CarouselContent>
-                    <>
-                        {projects
-                            .filter((project) => project.toFeature)
-                            .map((project: Project) => {
-                                return (
-                                    <CarouselItem>
-                                        <ProjectCard
-                                            key={project.id}
-                                            project={project}
-                                        />
-                                    </CarouselItem>
-                                );
-                            })}
-                    </>
-                </CarouselContent>
-
-                <CarouselNext />
+            {/* <Carousel slide={2} show={3} transition={0.5}> */}
+            <Carousel responsive={responsive}>
+                {projects
+                    .filter((project) => project.toFeature)
+                    .map((project: Project) => {
+                        return (
+                            <ProjectCard key={project.id} project={project} />
+                        );
+                    })}
             </Carousel>
+            {/* </Carousel> */}
         </Box>
     );
 }
